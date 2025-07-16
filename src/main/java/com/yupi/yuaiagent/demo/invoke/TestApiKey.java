f72f4b46120a910c60ba86bbe5b9cbd4549587ed
ziyifan <74227823+ziyif6@users.noreply.github.com>
2025-07-16 13:54:44 +0800

    apikey

--- src/main/java/com/yupi/yuaiagent/demo/invoke/TestApiKey.java
+++ src/main/java/com/yupi/yuaiagent/demo/invoke/TestApiKey.java
@@ -6,5 +6,5 @@ package com.yupi.yuaiagent.demo.invoke;
 public interface TestApiKey {
 
     // 修改为你的 API Key
-    String API_KEY = "修改为你的 API Key";
+    String API_KEY = "sk-4f3a60db61f14fa8a35988caf190dff2";
 }
